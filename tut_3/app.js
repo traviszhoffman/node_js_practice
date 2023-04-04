f1f82c858fc468bca4be65be3f241324fc00c8c1
Travis Hoffman <traviszh@byu.edu>
2023-04-04 12:31:23 -0600

    adding and using middleware & static files

--- tut_3/app.js
+++ tut_3/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 //express app
 const app = express();
@@ -11,6 +12,9 @@ app.set('view engine', 'ejs');
 
 //listen for requests
 app.listen(3000);
+//middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     const blogs = [
