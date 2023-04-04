6c26a5ec765f7f48e196b72f24ea997fdc211231
Travis Hoffman <traviszh@byu.edu>
2023-04-04 12:08:03 -0600

    changes using ejs

---
+++ tut_3/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+
+//express app
+const app = express();
+
+//register view egine
+app.set('view engine', 'ejs');
+//if need to set a file to different place
+//app.set('views', 'myviews');
+
+
+//listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      ];
+    res.render('index', {title: 'Home', blogs});
+    //res.sendFile('./views/index.html', { root: __dirname});
+    //res.send('<p>home page</p>');
+})
+
+app.get('/about', (req, res) => {
+    //res.send('<p>about page</p>');
+    //res.sendFile('./views/about.html', { root: __dirname});
+    res.render('about', {title: 'About'})
+})
+
+//redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+})
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'Create a new blog' });
+  });
+
+//404 page
+//for middlewar applications
+app.use( (req, res) => {
+    //res.status(404).sendFile('./views/404.html', { root: __dirname});
+    res.status(404).render('404', {title: '404'});
+})
