13d3d69399ecb0ae9602ab3f0456e551accf747d
Travis Hoffman <traviszh@byu.edu>
2023-04-05 14:28:51 -0600

    finished course

--- tut_3/app.js
+++ tut_3/app.js
@@ -1,50 +1,48 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes');
 
 //express app
 const app = express();
 
+//connect to mongodb
+const dbURI = '';
+
+mongoose.connect(dbURI, { useNewUrlParser: true,useUnifiedTopology: true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+    
 //register view egine
 app.set('view engine', 'ejs');
 //if need to set a file to different place
 //app.set('views', 'myviews');
 
 
-//listen for requests
-app.listen(3000);
 //middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
+app.use((req, res, next) => {
+    res.locals.path = req.path;
+    next();
+  });
 
-app.get('/', (req, res) => {
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index', {title: 'Home', blogs});
-    //res.sendFile('./views/index.html', { root: __dirname});
-    //res.send('<p>home page</p>');
-})
+// routes
+  app.get('/', (req, res) => {
+    res.redirect('/blogs');
+  });
 
 app.get('/about', (req, res) => {
-    //res.send('<p>about page</p>');
-    //res.sendFile('./views/about.html', { root: __dirname});
     res.render('about', {title: 'About'})
 })
 
-//redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-})
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', { title: 'Create a new blog' });
-  });
+//blog routes
+app.use('/blogs', blogRoutes);
 
 //404 page
 //for middlewar applications
 app.use( (req, res) => {
-    //res.status(404).sendFile('./views/404.html', { root: __dirname});
     res.status(404).render('404', {title: '404'});
 })
---
+++ tut_3/models/blog.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const blogSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    snippet: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+}, { timestamps: true });
+
+const Blog = mongoose.model('Blog', blogSchema);
+//make sure to name after collection that then becomes plural will looks for blogs collection
+module.exports = Blog;
